48775212693399bcf950ee86d34b081a2e9f8ee0
aso1201792 <1201792@st.asojuku.ac.jp>
2014-05-23 10:48:43 +0900

    5/23

--- gen/com/example/hellogalaxy/R.java
+++ gen/com/example/hellogalaxy/R.java
@@ -1094,7 +1094,7 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f050047;
+        public static final int btnOK=0x7f050047;
         public static final int button2=0x7f050046;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
@@ -1102,9 +1102,9 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int editText1=0x7f050041;
-        public static final int editText2=0x7f050044;
         public static final int edit_query=0x7f050030;
+        public static final int edtFamily=0x7f050044;
+        public static final int edtFirst=0x7f050041;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1147,9 +1147,9 @@ containing a value of this type.
         public static final int tableRow2=0x7f050042;
         public static final int textView1=0x7f050040;
         public static final int textView2=0x7f050043;
-        public static final int textView3=0x7f050048;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int txvMsg=0x7f050048;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
